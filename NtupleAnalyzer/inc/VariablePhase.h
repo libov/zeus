bde782837e709c7cf3708d76731cb5ddb41d9aeb
Vladyslav <libov@mail.desy.de>
2011-12-06 14:57:09 +0100

    code formatting

--- NtupleAnalyzer/inc/VariablePhase.h
+++ NtupleAnalyzer/inc/VariablePhase.h
@@ -1,12 +1,11 @@
 #ifndef __VarPhase__
 #define __VarPhase__
-enum	VariablePhase
-{
-	kEventVar,
-	kVertexVar,
-	kTrueVarEvent,
-	kTrueVarJet,
-        kRhoVar,
-        kRhoTrueVar
+enum VariablePhase {
+    kEventVar,
+    kVertexVar,
+    kTrueVarEvent,
+    kTrueVarJet,
+    kRhoVar,
+    kRhoTrueVar
 };
 #endif
