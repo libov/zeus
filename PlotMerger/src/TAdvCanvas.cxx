2bca984dda6a9165d41da7039da39aab50297a20
Vladyslav <libov@mail.desy.de>
2012-02-14 11:26:53 +0100

    TAdvCanvas.cxx: code formatting

--- PlotMerger/src/TAdvCanvas.cxx
+++ PlotMerger/src/TAdvCanvas.cxx
@@ -71,13 +71,12 @@ Bool_t		TAdvCanvas::LogPad(Int_t	Pad)
 }
 TCanvas* TAdvCanvas::CreateCanvas() {
 
-        TCanvas *fCanvas = new TCanvas(fCanvName, "Control Plots", fWidth, fHeight);
+    TCanvas *fCanvas = new TCanvas(fCanvName, "Control Plots", fWidth, fHeight);
 
-   //gStyle->SetOptFit(1111);
-   fCanvas->Range(0.275,-234.806,5.525,2113.26);
-   fCanvas->SetBorderSize(2);
-	fCanvas->SetFrameFillColor(0);
-	fCanvas->SetFillColor(0);
+    fCanvas->Range(0.275,-234.806,5.525,2113.26);
+    fCanvas->SetBorderSize(2);
+    fCanvas->SetFrameFillColor(0);
+    fCanvas->SetFillColor(0);
    fCanvas->ToggleToolBar();
 	fCanvas->ToggleEventStatus();
 	fCanvas->ToggleEditor();
