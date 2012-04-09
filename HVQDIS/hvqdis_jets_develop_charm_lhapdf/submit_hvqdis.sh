a80ddb20005ccb62d660040b87adc2e739478ac6
Vladyslav <libov@mail.desy.de>
2012-04-09 11:51:52 +0200

    changed jobsub to zarah-jobsub; changed the executable name; removed .tbl files, as now LHAPDF is used

--- HVQDIS/hvqdis_jets_develop_charm_lhapdf/submit_hvqdis.sh
+++ HVQDIS/hvqdis_jets_develop_charm_lhapdf/submit_hvqdis.sh
@@ -7,5 +7,5 @@ else
   queue=$2
  fi
  folder=$1
- jobsub -h zenithsub -q $queue $folder/run_farm.sh -f $folder/hvqben.x $folder/run_hvqdis.sh $folder/cteq5f4.tbl  $folder/cteq5f3.tbl 
+ zarah-jobsub -q $queue -s $folder/run_farm.sh exe/hvq.x $folder/run_hvqdis.sh $ANALYSIS/env.sh
 fi
