43d48dc287147bb8734a989f44732e842f57eb1a
Vladyslav <libov@mail.desy.de>
2012-04-09 11:52:41 +0200

    adding source env.sh line so that path to lhapdf library is specified; removed ben suffices

--- HVQDIS/hvqdis_jets_develop_charm_lhapdf/hvqdis_farm/run_hvqdis.sh
+++ HVQDIS/hvqdis_jets_develop_charm_lhapdf/hvqdis_farm/run_hvqdis.sh
@@ -1,18 +1,18 @@
-
-#!/usr/bin/bash
+#!/bin/bash
 #
 #	Run HVQDIS with different steering files
 #	by PK
 #
 #echo '*** Running HVQDIS 1.4 ***'
+source env.sh
 if [ -z $1 ]; then
   echo usage $0 nametag
   exit
 else
   tag=$1
 fi
-output=ben_${tag}.out
-time ./hvqben.x < hvqdis_${tag}.dat > ${output}
+output=${tag}.out
+time ./hvq.x < hvqdis_${tag}.dat > ${output}
 mv ${output} ./output/
 histoname=`grep histos hvqdis_${tag}.dat | awk -F \' '{print $2}'`
 mv $histoname ./Histograms/
