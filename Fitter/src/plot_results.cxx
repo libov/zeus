d47352396542f377e724746ace84868a382b3370
Vladyslav <libov@mail.desy.de>
2012-06-04 15:22:09 +0200

    plot_results.cxx: added a possibility to draw also a 3rd XML file, which is assumed to be additional set of predictions

--- Fitter/src/plot_results.cxx
+++ Fitter/src/plot_results.cxx
@@ -26,13 +26,16 @@ int main(int argc, char **argv) {
     static struct option long_options[] = {
         {"file1", required_argument, 0, 1},
         {"file2", required_argument, 0, 2},
+        {"file3", required_argument, 0, 3}
     };
 
     // results of the command line option processing will be stored here
     TString binningXMLfileName1;
     TString binningXMLfileName2;
+    TString binningXMLfileName3;
     bool    first_file_given = false;
     bool    second_file_given = false;
+    bool    third_file_given = false;
     bool    plot_beauty = false;    // default: plot charm
 
     // loop over program arguments (i.e. argv array) and store info to above variables
@@ -49,12 +52,16 @@ int main(int argc, char **argv) {
                 binningXMLfileName2 = optarg;
                 second_file_given = true;
                 break;
+            case 3:
+                binningXMLfileName3 = optarg;
+                third_file_given = true;
+                break;
             case 'b':
                 plot_beauty = true;
                 break;
             case 'h':
                 cout << "\nUsage:\n\n";
-                cout << "\tplot_results --file1 <xmlfile1> [--file2 <xmlfile1>] [-b] [-h]\n\n";
+                cout << "\tplot_results --file1 <xmlfile1> [--file2 <xmlfile2>] [--file3 <xmlfile3>] [-b] [-h]\n\n";
                 cout << "\t This application is designed to visualize cross-sections that are stored in XML files.\n";
                 cout << "\t If two files are given (i.e. both options --file1 and --file2 are specified), a ratio plot will be shown below the main graph.\n";
                 cout << "\t The functionality of the underlying class TResultPlotter allows any canvas layout and any number of XML files, but that is not implemented in this application.\n";
@@ -65,6 +72,7 @@ int main(int argc, char **argv) {
                 cout << "\t-> data-to-theory comparisons: 1st file is data, 2nd file is theory\n";
                 cout << "\t-> ratio plot always shows 1st divided by 2nd, which makes sense for both of the two conventions above\n";
                 cout << "\t-> 1st-file-plot will appear on top of the 2nd, which makes sense for the data-to-theory convention\n";
+                cout << "\t*** EDIT: A THIRD FILE IS NOW POSSIBLE, ASSUMED TO BE OTHER PREDICTION ***\n";
                 cout << "\n\tOptions:\n";
                 cout << "\t-b\tPlot beauty results; otherwise - charm\n";
                 cout << "\t-h\tPrint this help and exit\n\n";
@@ -119,13 +127,16 @@ int main(int argc, char **argv) {
     // add XML files, where results to be displayed are
     if (first_file_given) cResultPlotter.AddXMLFile(binningXMLfileName1);
     if (second_file_given) cResultPlotter.AddXMLFile(binningXMLfileName2);
+    if (third_file_given) cResultPlotter.AddXMLFile(binningXMLfileName3);
 
     // draw plots; file1 should be on top of the file2, hence draw file2 first
     if (second_file_given) cResultPlotter.DrawPlots(binningXMLfileName2, 1, false, 2);
+    if (third_file_given) cResultPlotter.DrawPlots(binningXMLfileName3, 1, true, 2);
     if (first_file_given) cResultPlotter.DrawPlots(binningXMLfileName1, 1, true, 1);
 
     // the ratio plot
     if (first_file_given && second_file_given) cResultPlotter.DrawRatio(binningXMLfileName1, binningXMLfileName2, 2, false);
+    if (second_file_given && third_file_given) cResultPlotter.DrawRatio(binningXMLfileName3, binningXMLfileName2, 3, false);
 
     // save results
     cResultPlotter.PrintCanvases();
