6f25a83441c147c84f663a88cc06e0e27be61738
Vladyslav <libov@mail.desy.de>
2012-06-05 11:42:26 +0200

    added another quick script directory - for multiplication of the uncorrected theory predictions by hadronization and qed factors - abkm charm Philipp - and the output of the  script

---
+++ other/predictions_Philipp/charm_ABKM.cxx
@@ -0,0 +1,50 @@
+int charm_ABKM() {
+
+    cout << "ABKM predictions from Philipp, corrected by hadr and qed effects. see mail from Philipp 04 June 2012" << endl;
+
+    cout << "\n eta " << endl;
+    
+    cout << 1771.74 * 0.89 * 0.99 << endl;
+    cout << 2830.24 * 0.97 * 0.98 << endl;
+    cout << 3424.51 * 1.02 * 0.98 << endl;
+    cout << 3925.19 * 1.05 * 0.98 << endl;
+    cout << 4220.68 * 1.07 * 0.98 << endl;
+    cout << 4360.79 * 1.10 * 0.98 << endl;
+    cout << 4296.14 * 1.11 * 0.98 << endl;
+    cout << 3997.26 * 1.10 * 0.98 << endl;
+    cout << 3610.13 * 1.09 * 0.98 << endl;
+    cout << 3259.53 * 1.07 * 0.97 << endl;
+    cout << 2373.41 * 1.13 * 0.97 << endl;
+    
+    cout << "\n et " << endl;
+    
+    cout << 2550.57 * 1.06 * 0.98 << endl;
+    cout << 549.657 * 1.05 * 0.97 << endl;
+    cout << 179.623 * 1.03 * 0.96 << endl;
+    cout << 69.3129 * 0.99 * 0.93 << endl;
+    cout << 30.7936 * 0.96 * 0.93 << endl;
+    cout << 11.3425 * 0.95 * 0.85 << endl;
+    cout << 2.22761 * 0.86 * 0.88 << endl;
+    
+    cout << "\n x " << endl;
+    
+    cout << 7.50814e+06 * 1.19 * 0.96 << endl;
+    cout << 6.89291e+06 * 1.20 * 0.98 << endl;
+    cout << 3.35909e+06 * 1.09 * 0.99 << endl;
+    cout << 979320 * 0.97 * 0.99 << endl;
+    cout << 243357 * 0.91 * 1.00 << endl;
+    cout << 9405.97 * 0.88 * 0.88 << endl;
+    
+    cout << "\n q2 " << endl;
+    
+    cout << 630.167 * 1.15 * 0.98 << endl;
+    cout << 316.031 * 1.08 * 0.99 << endl;
+    cout << 140.209 * 1.01 * 0.98 << endl;
+    cout << 52.84 * 1.00 * 0.97 << endl;
+    cout << 18.204 * 1.00 * 0.97 << endl;
+    cout << 5.78999 * 1.01 * 0.96 << endl;
+    cout << 1.36111 * 1.01 * 0.95 << endl;
+    cout << 0.162799 * 1.02 * 0.87  << endl;
+
+    return 0;
+}
