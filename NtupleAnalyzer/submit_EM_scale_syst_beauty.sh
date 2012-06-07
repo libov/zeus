9614f8e44c3388d44acc4831971bdec948c6ea41
Vladyslav <libov@mail.desy.de>
2012-06-07 13:56:33 +0200

    added submission scripts for EM scale systematic studies for charm and beauty

---
+++ NtupleAnalyzer/submit_EM_scale_syst_beauty.sh
@@ -0,0 +1,7 @@
+./submit_analysis -b full -v 2.57.1 --do_EM_scale_syst --EMScaleUncertainty -0.015
+./submit_analysis -b full -v 2.57.2 --do_EM_scale_syst --EMScaleUncertainty -0.01
+./submit_analysis -b full -v 2.57.3 --do_EM_scale_syst --EMScaleUncertainty -0.005
+./submit_analysis -b full -v 2.57.4 --do_EM_scale_syst --EMScaleUncertainty 0
+./submit_analysis -b full -v 2.57.5 --do_EM_scale_syst --EMScaleUncertainty 0.005
+./submit_analysis -b full -v 2.57.6 --do_EM_scale_syst --EMScaleUncertainty 0.01
+./submit_analysis -b full -v 2.57.7 --do_EM_scale_syst --EMScaleUncertainty 0.015
---
+++ NtupleAnalyzer/submit_EM_scale_syst_charm.sh
@@ -0,0 +1,7 @@
+./submit_analysis -b full.forCHARM -v 2.56.1 --do_EM_scale_syst --EMScaleUncertainty -0.015
+./submit_analysis -b full.forCHARM -v 2.56.2 --do_EM_scale_syst --EMScaleUncertainty -0.01
+./submit_analysis -b full.forCHARM -v 2.56.3 --do_EM_scale_syst --EMScaleUncertainty -0.005
+./submit_analysis -b full.forCHARM -v 2.56.4 --do_EM_scale_syst --EMScaleUncertainty 0
+./submit_analysis -b full.forCHARM -v 2.56.5 --do_EM_scale_syst --EMScaleUncertainty 0.005
+./submit_analysis -b full.forCHARM -v 2.56.6 --do_EM_scale_syst --EMScaleUncertainty 0.01
+./submit_analysis -b full.forCHARM -v 2.56.7 --do_EM_scale_syst --EMScaleUncertainty 0.015
