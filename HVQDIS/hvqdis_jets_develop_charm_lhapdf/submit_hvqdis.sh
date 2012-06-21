9cd20d0485531a5ecf6dabf47584324724c35bd5
Vladyslav <libov@mail.desy.de>
2012-06-21 08:14:38 +0200

    code format

--- HVQDIS/hvqdis_jets_develop_charm_lhapdf/submit_hvqdis.sh
+++ HVQDIS/hvqdis_jets_develop_charm_lhapdf/submit_hvqdis.sh
@@ -61,8 +61,8 @@ function setDefaults()
     NPTS1=800000                #!NUMBER OF VEGAS POINTS FOR NLO
     ITS1=4                      #!NUMBER OF VEGAS ITERATIONS FOR NLO
     if [ $FLAVOR == charm ] ; then
-        IQUARK=1                    #!1:CHARM  2:BOTTOM
-        XM=1.5                      #!QUARK MASS
+        IQUARK=1                #!1:CHARM  2:BOTTOM
+        XM=1.5                  #!QUARK MASS
     elif [ $FLAVOR == beauty ] ; then
         IQUARK=2                    #!1:CHARM  2:BOTTOM
         XM=4.75                      #!QUARK MASS
