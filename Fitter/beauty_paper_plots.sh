a9859ac737d1baa82e1b5de3db2b6a6ada81e900
Vladyslav <libov@mail.desy.de>
2012-07-25 13:45:35 +0200

    added bash scripts to create plots and text results for the paper - in order to avoid any errors due to that

---
+++ Fitter/beauty_paper_plots.sh
@@ -0,0 +1,2 @@
+./plot_results --file1 results.full2.53.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_beauty_2.47.SYST --file2 hvqdis_predictions.meta_Jun21_2012_09:45:22.xml --file3 hvqdis_predictions.meta_Jun21_2012_10:09:34.xml -b
+./result_printer --file results.full2.53.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_beauty_2.47.SYST --beauty
---
+++ Fitter/charm_paper_plots.sh
@@ -0,0 +1,2 @@
+./plot_results --file1 results.full.forCHARM2.52.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_charm_2.46.SYST --file2 hvqdis_predictions.meta_Jun21_2012_09:44:53.xml --file3 hvqdis_predictions.meta_Jun21_2012_10:09:19.xml
+./result_printer --file results.full.forCHARM2.52.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_charm_2.46.SYST
