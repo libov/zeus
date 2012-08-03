e9bd935484333536908339304c56896b43ef2a70
Vladyslav <libov@mail.desy.de>
2012-08-03 10:00:05 +0200

    added significance printing to paper_plots.sh script

--- PlotMerger/paper_plots.sh
+++ PlotMerger/paper_plots.sh
@@ -1,3 +1,5 @@
 ./plotter -b full.forCHARM -v 2.52.2 -d -s --config_file F2bc_paper_charm --no_indices
 
 ./plotter -b full -v 2.53.2 -d -s --config_file F2bc_paper_beauty --no_indices
+
+./plotter -b full.forCHARM -v 2.52.2 -d -s --config_file F2bc_paper_significance
