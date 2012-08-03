732ae1043a1d836b3e544a53ed3134bf1e02a009
Vladyslav <libov@mail.desy.de>
2012-08-03 14:08:30 +0200

    added production of mirrored significance plot to paper script

--- PlotMerger/paper_plots.sh
+++ PlotMerger/paper_plots.sh
@@ -3,3 +3,5 @@
 ./plotter -b full -v 2.53.2 -d -s --config_file F2bc_paper_beauty --no_indices
 
 ./plotter -b full.forCHARM -v 2.52.2 -d -s --config_file F2bc_paper_significance
+
+./plotter -b full.forCHARM -v 2.52.2 -d -s --config_file F2bc_paper_significance_mirrored
