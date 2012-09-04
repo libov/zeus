6a8f0c036278221fe4aa17e4df259a21998afcce
Vladyslav <libov@mail.desy.de>
2012-09-04 12:10:04 +0200

    added a separate file with constants

---
+++ NtupleAnalyzer/inc/constants.h
@@ -0,0 +1,14 @@
+#ifndef __CONSTANTS_H__
+#define __CONSTANTS_H__
+
+namespace constants {
+
+    const Double_t M_PROTON = 0.938;
+    const Double_t M_ELECTRON = 0.000511;
+    const Double_t M_PION = 0.139570;
+
+    const Double_t E_BEAM = 27.5;
+    const Double_t E_PROTON = 920;
+}
+
+#endif
