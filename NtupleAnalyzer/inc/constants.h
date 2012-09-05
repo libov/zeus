a7acdfb05868b4d5057b5f3c46242043d1f842b2
Vladyslav <libov@mail.desy.de>
2012-09-05 13:55:54 +0200

    added kaon mass to constants.h

--- NtupleAnalyzer/inc/constants.h
+++ NtupleAnalyzer/inc/constants.h
@@ -6,6 +6,7 @@ namespace constants {
     const Double_t M_PROTON = 0.938;
     const Double_t M_ELECTRON = 0.000511;
     const Double_t M_PION = 0.139570;
+    const Double_t M_KAON = 0.4937;
 
     const Double_t E_BEAM = 27.5;
     const Double_t E_PROTON = 920;
