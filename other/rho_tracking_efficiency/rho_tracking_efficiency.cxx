798656c0758fa752ea44f8c072e7b190f6a0fa4c
Vladyslav <libov@mail.desy.de>
2012-09-06 09:33:39 +0200

    significantly reworked rho tracking efficiency script

--- other/rho_tracking_efficiency/rho_tracking_efficiency.cxx
+++ other/rho_tracking_efficiency/rho_tracking_efficiency.cxx
@@ -3,155 +3,173 @@
 #include <TString.h>
 #include <TFile.h>
 #include <TH1F.h>
+#include <TF1.h>
 #include <TCanvas.h>
 #include <TLine.h>
+#include <TMath.h>
+#include <TStyle.h>
+#include <TPaveStats.h>
 
-// input data
+// settings - not changed often, hence implementing as constants, not passing as arguments
 const TString   BINNING = "inclusive";
-const TString   HISTO_VERSION = "rho.v103";
 const TString   REMARK = ".0405e06e07p";
 const TString   BIN = "bin1";
-const TString   OUTPUT_FILENAME = "pion_pt_reweighting_histo";
+const Double_t  PT_STEP_POINT = 1.5;
 
-// other global constants
-TString   HISTO_PATH = getenv("HISTO_PATH");
-TString   FILENAME = "merged." + BINNING + HISTO_VERSION + REMARK + ".root";
-TFile*    file = new TFile(HISTO_PATH + "/" + FILENAME, "read");
+// environment variables
+const TString HISTO_PATH = getenv("HISTO_PATH");
+const TString PLOTS_PATH = getenv("PLOTS_PATH");
 
-void helper2(TString variable);
+// other global variables
+TFile*    file;
+TString   HISTO_VERSION;
 
-void rho_tracking_efficiency() {
-
-    helper2("pt");
-    helper2("phi");
-    helper2("theta");
-
+// a step function
+Double_t step_function (Double_t * x, Double_t * par) {
+    if ( x[0] <= PT_STEP_POINT ) return par[0];
+    if ( x[0] > PT_STEP_POINT ) return par[1];
 }
 
-void helper2(TString variable) {
+// initializer
+void Initialize() {
+    TString   FILENAME = "merged." + BINNING + HISTO_VERSION + REMARK + ".root";
+    file = new TFile(HISTO_PATH + "/" + FILENAME, "read");
 
-    TCanvas * c1 = new TCanvas();
-    c1 -> Divide(2,2);
+    gStyle->SetErrorX(0);
+    gStyle -> SetFrameBorderMode(0);
+    gStyle -> SetOptStat(0);
+    gStyle -> SetOptFit(1);
+    gStyle -> SetPadLeftMargin(0.135);
+    gStyle -> SetStatColor(0);
+    Float_t right_margin = gStyle->GetPadRightMargin();
+    Float_t top_margin = gStyle->GetPadTopMargin();
+    gStyle -> SetStatX( 1 - right_margin );
+    gStyle -> SetStatY( 1 - top_margin );
+}
 
-    // data: ratio ztt/(ztt+msa) for pion pt
-    TH1F * pi_pt_ZTTMSA_data = (TH1F*) file -> Get(BIN+"/pi_"+variable+"_ZTTMSA/data");
-    TH1F * pi_pt_MSA_data = (TH1F*) file -> Get(BIN+"/pi_"+variable+"_MSA/data");
-    TH1F * pi_pt_ratio_data = (TH1F*) pi_pt_MSA_data -> Clone("pi_"+variable+"_ratio_data");
-    pi_pt_ratio_data -> Sumw2();
-    pi_pt_ratio_data -> Divide(pi_pt_ZTTMSA_data);
+void cosmetics(TH1F* h, TString x_title, TString y_title) {
+    h -> SetAxisRange(0, 0.03, "Y");
+    h -> SetXTitle(x_title);
+    h -> SetYTitle(y_title);
+    h -> SetTitleOffset(1.35, "Y");
+    h -> SetTitleSize(0.05, "Y");
+    h -> SetMarkerStyle(20);
+}
+
+// a plotting routine - produces ratio and double ratio plots for a given variable
+void make_plots(TString variable) {
+
+    // x-axis title
+    TString Xtitle;
+    Float_t x_range_low;
+    Float_t x_range_up;
+    if (variable == "pt") {
+        Xtitle = "track p_{T} (GeV)";
+        x_range_low = 0;
+        x_range_up = 5;
+    } else if (variable == "phi") {
+        Xtitle = "track #phi (rad)";
+        x_range_low = -TMath::Pi();
+        x_range_up = TMath::Pi();
+    } else if (variable == "theta") {
+        x_range_low = 0;
+        x_range_up = TMath::Pi();
+        Xtitle = "track #theta (rad)";
+    }
+
+    // create a canvas for the single ratio (R)
+    TCanvas * c1 = new TCanvas("c1", "Ratio", 750, 350);
+    c1 -> Divide(2,1);
+
+    // data: ratio ztt/(ztt+msa)
+    TH1F * pi_ZTTMSA_data = (TH1F*) file -> Get(BIN+"/pi_"+variable+"_ZTTMSA/data");
+    TH1F * pi_MSA_data = (TH1F*) file -> Get(BIN+"/pi_"+variable+"_MSA/data");
+    TH1F * pi_ratio_data = (TH1F*) pi_MSA_data -> Clone("pi_"+variable+"_ratio_data");
+    pi_ratio_data -> Sumw2();
+    pi_ratio_data -> Divide(pi_ZTTMSA_data);
     c1 -> cd(1);
-    pi_pt_ratio_data -> Draw();
-    pi_pt_ratio_data -> SetAxisRange(0, 0.03, "Y"); 
+    pi_ratio_data -> Draw();
+    cosmetics(pi_ratio_data, Xtitle, "R_{DATA}");
 
-    // mc: ratio ztt/(ztt+msa) for pion pt
-    TH1F * pi_pt_ZTTMSA_mc = (TH1F*) file -> Get(BIN+"/pi_"+variable+"_ZTTMSA/rho");
-    TH1F * pi_pt_MSA_mc= (TH1F*) file -> Get(BIN+"/pi_"+variable+"_MSA/rho");
-    TH1F * pi_pt_ratio_mc = (TH1F*) pi_pt_MSA_mc -> Clone("pi_"+variable+"_ratio_mc");
-    pi_pt_ratio_mc -> Sumw2();
-    pi_pt_ratio_mc -> Divide(pi_pt_ZTTMSA_mc);
+    // mc: ratio ztt/(ztt+msa)
+    TH1F * pi_ZTTMSA_mc = (TH1F*) file -> Get(BIN+"/pi_"+variable+"_ZTTMSA/rho");
+    TH1F * pi_MSA_mc= (TH1F*) file -> Get(BIN+"/pi_"+variable+"_MSA/rho");
+    TH1F * pi_ratio_mc = (TH1F*) pi_MSA_mc -> Clone("pi_"+variable+"_ratio_mc");
+    pi_ratio_mc -> Sumw2();
+    pi_ratio_mc -> Divide(pi_ZTTMSA_mc);
     c1 -> cd(2);
-    pi_pt_ratio_mc -> Draw();
-    pi_pt_ratio_mc -> SetAxisRange(0, 0.03, "Y");
+    pi_ratio_mc -> Draw();
+    cosmetics(pi_ratio_mc, Xtitle, "R_{MC}");
 
-    // double ratio: data/mc for pion pt
-    TH1F * pi_pt_ratio = (TH1F*)pi_pt_ratio_data -> Clone("pi_"+variable+"_ratio");
-    pi_pt_ratio -> Sumw2();
-    pi_pt_ratio -> Divide(pi_pt_ratio_mc);
-    c1 -> cd(3);
-    pi_pt_ratio -> Draw();
-    pi_pt_ratio -> SetAxisRange(0, 2, "Y");
-    TLine * line = new TLine(0, 1, 5, 1);
-    line -> Draw("same");
-    line -> SetLineStyle(2);
+    // create a canvas for the double ratio (epsilon)
+    TCanvas * c2 = new TCanvas("c2", "Double Ratio", 700, 700);
 
-    TString PLOTS_PATH = getenv("PLOTS_PATH");
-    c1 -> Print(PLOTS_PATH + "/" + BINNING+HISTO_VERSION+REMARK+BIN+"_"+variable+".eps");
+    // double ratio: data/mc
+    TH1F * pi_ratio = (TH1F*)pi_ratio_data -> Clone("pi_"+variable+"_ratio");
+    pi_ratio -> Sumw2();
+    pi_ratio -> Divide(pi_ratio_mc);
+    c2 -> cd();
+    pi_ratio -> Draw();
+    pi_ratio -> SetAxisRange(0, 3.4, "Y");
+    pi_ratio -> SetYTitle("#epsilon = R_{DATA}/R_{MC}");
+    pi_ratio -> SetTitleOffset(1.2, "Y");
+    pi_ratio -> SetXTitle(Xtitle);
+    pi_ratio -> SetMarkerStyle(20);
+
+    if ( variable == "theta" || variable == "phi") pi_ratio -> Fit("pol0", "", "", x_range_low, x_range_up );
+    if ( variable == "pt" ) {
+
+        TF1 * step_fcn = new TF1("step", step_function, x_range_low, x_range_up, 2);
+        step_fcn -> SetParameters(1, 1);
+        step_fcn -> SetNpx(10000);
+        pi_ratio -> Fit("step");
+    }
+
+    c1 -> Print(PLOTS_PATH + "/" + BINNING+HISTO_VERSION+REMARK+BIN+"_"+variable+"_R.eps");
+    c2 -> Print(PLOTS_PATH + "/" + BINNING+HISTO_VERSION+REMARK+BIN+"_"+variable+"_epsilon.eps"); 
 }
 
-void pt_theta_reweighting_v1() {
+// highest-level routine
+void rho_tracking_efficiency(TString version="rho.v136") {
 
-    TString FILENAME1 = "analysis.mc07pv06b.f8196.rho_." + BINNING + HISTO_VERSION + ".root";
-    TString FILENAME2 = "analysis.data06pv06a." + BINNING + HISTO_VERSION + ".root";
+    HISTO_VERSION = version;
+    Initialize();
 
-    TFile   *f1 = new TFile(HISTO_PATH + "/" + FILENAME1, "read");
-    TFile   *f2 = new TFile(HISTO_PATH + "/" + FILENAME2, "read");
-
-    TH1F * h_mc = (TH1F*) f1 -> Get("fMc_pt_theta_pi");    
-    TH1F * h_data = (TH1F*) f2 -> Get("fMc_pt_theta_pi");
-
-    TH1F * h_ratio = (TH1F *) h_data -> Clone ("ratio");
-    h_ratio -> Divide(h_mc);
-    h_ratio -> Draw();
+    make_plots("pt");
+    make_plots("phi");
+    make_plots("theta");
 }
 
-void helper1(TCanvas * c, TString bin, int pad1, TString variable) {
-    TH1F * h_mc = (TH1F*) file -> Get(bin+"/pi_"+variable+"_ZTT/rho");
-    TH1F * h_data = (TH1F*) file -> Get(bin+"/pi_"+variable+"_ZTT/data");
-    c -> cd(pad1);
-    h_mc -> Draw("hist");
-    h_data -> Draw("same");
-    h_mc -> SetFillColor(kYellow);
-
-    c -> cd (++pad1);
-    TH1F * h_ratio = (TH1F *) h_data -> Clone ("ratio");
-    h_ratio -> Divide(h_mc);
-    h_ratio -> Draw();
-    h_ratio -> SetAxisRange(0, 5, "Y");
-
-    TLine * line = new TLine(0, 1, 5, 1);
-    line -> Draw("same");
-    line -> SetLineStyle(2);
-}
-
-void pt_theta_reweighting_v2() {
+// determines reweighting parameters
+void reweighting (TString version, TString variable, TString fitfcn, Double_t low_x, Double_t up_x, TString xtitle, Double_t low_y, Double_t up_y) {
 
     TCanvas * c = new TCanvas();
-    c -> Divide (4,4);
 
-    helper1(c, "bin1", 1, "pt");
-    helper1(c, "bin2", 3, "pt");
-    helper1(c, "bin3", 5, "pt");
-    helper1(c, "bin4", 7, "pt");
-    helper1(c, "bin5", 9, "pt");
-    helper1(c, "bin6", 11, "pt");
-    helper1(c, "bin7", 13, "pt");
-}
+    HISTO_VERSION = version;
+    Initialize();
 
-void store_reweighting_histo () {
-
-    TH1F * h_mc = (TH1F*) file -> Get(BIN+"/pi_pt_ZTT/rho");
-    TH1F * h_data = (TH1F*) file -> Get(BIN+"/pi_pt_ZTT/data");
+    TH1F * h_mc = (TH1F*) file -> Get(BIN+"/"+variable+"/rho");
+    TH1F * h_data = (TH1F*) file -> Get(BIN+"/"+variable+"/data");
     TH1F * h_ratio = (TH1F *) h_data -> Clone ("ratio");
     h_ratio -> Divide(h_mc);
 
-    TString DATABASE_PATH = getenv("DATABASE_PATH");
-//  Note: commented out so that it's not overwritten by chance
-//     TFile * file_out = new TFile (DATABASE_PATH + "/" + OUTPUT_FILENAME + ".root", "recreate");
-//     file_out -> cd();
-//     h_ratio -> Write();
-//     file_out -> Close();
+    h_ratio -> SetXTitle(xtitle);
+    h_ratio -> SetAxisRange(low_y, up_y, "Y");
+    h_ratio -> SetMarkerStyle(20);
+    h_ratio -> SetYTitle("data/MC");
+    gStyle -> SetStatColor(0);
+    gStyle -> SetStatW(0.1);
+    gStyle -> SetStatH(0.1);
 
-}
-
-void phi_theta_reweighting_v2() {
-
-    TCanvas * c = new TCanvas();
-    c -> Divide (4,4);
-
-    helper1(c, "bin1", 1, "phi");
-    helper1(c, "bin2", 3, "phi");
-    helper1(c, "bin3", 5, "phi");
-    helper1(c, "bin4", 7, "phi");
-    helper1(c, "bin5", 9, "phi");
-    helper1(c, "bin6", 11, "phi");
-    helper1(c, "bin7", 13, "phi");
-}
-
-void theta_star_reweighting() {
-
-    TH1F * h_mc = (TH1F*) file -> Get(BIN+"/theta_star_classI/rho");
-    TH1F * h_data = (TH1F*) file -> Get(BIN+"/theta_star_classI/data");
-    TH1F * h_ratio = (TH1F *) h_data -> Clone ("ratio");
-    h_ratio -> Divide(h_mc);
     h_ratio -> Draw();
+
+    h_ratio -> Fit(fitfcn, "", "", low_x, up_x);
+
+    c -> Print(PLOTS_PATH + "/" + BINNING+HISTO_VERSION+REMARK+BIN+"_"+variable+".eps");
+}
+
+// highest-level routine for reweighting parameters determination
+void all_reweightings() {
+    reweighting("rho.v134", "rho_phi_classI", "pol6", -3.14, 3.14, "#phi(#rho), rad", 0.4, 2);
+    reweighting("rho.v135", "cos_theta_star_classI", "pol2", -1, 1, "cos(#theta^{*})", 0, 7);
 }
