45f3814c614dc16ef84d52513c831207941e07e6
Vladyslav <libov@mail.desy.de>
2012-09-07 14:12:29 +0200

    rearranged the structure of the code

--- NtupleAnalyzer/src/TMiniNtupleAnalyzerTrackingEfficiency.cxx
+++ NtupleAnalyzer/src/TMiniNtupleAnalyzerTrackingEfficiency.cxx
@@ -19,244 +19,6 @@ using namespace std;
 #include <inc/TGlobalBin.h>
 #include <inc/TVertex.h>
 
-Double_t TMiniNtupleAnalyzer::getElectronEnergyConstrainedMethod(TLorentzVector rho) {
-
-    // use rho information to calculate scattered electron enegry  - "constrained" method, see e.g. eur phys c 6, 603-627 (1999)
-
-    // calculate e-pZ of the rho
-    Double_t    empz = rho.E() - rho.Pz();
-
-    // calculate electron energy
-    Double_t    E_elec = (2*E_BEAM - empz)/(1 - cos(Sith[0]));
-
-    // done
-    return E_elec;
-}
-
-TLorentzVector TMiniNtupleAnalyzer::getElectronConstrainedMethod(TLorentzVector rho) {
-
-    // get electron energy from constrained method
-    Double_t    E_elec = getElectronEnergyConstrainedMethod(rho);
-
-    // set scattered electron parameters
-    TLorentzVector electron; 
-    electron.SetXYZM(E_elec*sin(Sith[0])*cos(Siph[0]), E_elec*sin(Sith[0])*sin(Siph[0]), E_elec*cos(Sith[0]), M_ELECTRON);
-
-    return electron;
-}
-
-Double_t TMiniNtupleAnalyzer::getQ2ConstrainedMethod(TLorentzVector rho) {
-
-    // get electron energy from constrained method
-    Double_t    E_elec = getElectronEnergyConstrainedMethod(rho);
-
-    // calculate Q2
-    Double_t    q2 = 2 * E_BEAM * E_elec * ( 1 + cos(Sith[0]) );
-
-    // done
-    return q2;
-}
-
-Double_t TMiniNtupleAnalyzer::get_PHI_h(TLorentzVector rho, bool q_reco) {
-
-    // set incoming/outgoing lepton 4vectors
-    TLorentzVector  e_incoming(0, 0, (-1)*E_BEAM, sqrt(E_BEAM*E_BEAM + M_ELECTRON * M_ELECTRON));
-    TLorentzVector  e_outgoing;
-    if (q_reco) {
-        e_outgoing = getElectronConstrainedMethod(rho);
-    } else {
-        e_outgoing.SetXYZM(Plepton[0], Plepton[1], Plepton[2], M_ELECTRON);
-    }
-
-    // set the incoming proton 4-vector
-    TLorentzVector p_incoming(0, 0, E_PROTON, sqrt( E_PROTON * E_PROTON + M_PROTON * M_PROTON ) );
-
-    // get boost and rotation vectors as well as the gamma* vector
-    get_gammaP_boost(q_reco, true);
-
-    // boost leptons, incoming proton and the rho to the gamma*-p frame
-    e_incoming.Boost(fBoost);
-    e_incoming.Rotate(fAngle, fRotationAxis);
-
-    e_outgoing.Boost(fBoost);
-    e_outgoing.Rotate(fAngle, fRotationAxis);
-
-    p_incoming.Boost(fBoost);
-    p_incoming.Rotate(fAngle, fRotationAxis);
-
-    rho.Boost(fBoost);
-    rho.Rotate(fAngle, fRotationAxis);
-
-    // get normals to the scattering and production planes
-    TVector3 e_incoming_3vect = e_incoming.Vect();
-    TVector3 e_outgoing_3vect = e_outgoing.Vect();
-    TVector3 scat_plane_perp = e_incoming_3vect.Cross(e_outgoing_3vect);
-
-    TVector3 gamma_3vect = fq.Vect();
-    TVector3 rho_3vect = rho.Vect();
-    TVector3 prod_plane_perp = gamma_3vect.Cross(rho_3vect);
-
-    // these vectors are perpendicular to the Z axis (incoming proton/gamma* direction),
-    // hence can work in 2D space - that is in XY plane of the gamma*-p frame
-    TVector2 vscat = scat_plane_perp.XYvector();
-    TVector2 vprod = prod_plane_perp.XYvector();
-
-    // rotate both vectors in such a way that vscat has phi = 0
-    vprod=vprod.Rotate((-1)*vscat.Phi());
-    vscat=vscat.Rotate((-1)*vscat.Phi());
-
-    // the PHI_h is just the phi angle of vprod (range [0, 2pi], see ROOT docs)
-    Double_t PHI_h=vprod.Phi();
-
-    // sanity check
-    if ( TMath::IsNaN(PHI_h) ) {
-        cout << "ERROR: helicity angle is NAN" << endl;
-        abort();
-    }
-
-    // the other - equivalent way to define PHI:
-    // very similar to the definition in NUCL PHYS B 61 381-413 (1973),
-    // except a small thing, see below
-
-    TVector3 z = fq.Vect();
-    z = z.Unit();
-
-    TVector3 y = z.Cross(rho_3vect);
-    y = y.Unit();
-
-    TVector3 x = y.Cross(z);
-
-    TVector3 scat_plane_norm = e_incoming_3vect.Cross(e_outgoing_3vect);
-    scat_plane_norm = scat_plane_norm.Unit();
-
-    // the commented line is according to the paper; however
-    // in this case PHI is [0, pi] and not [0, 2pi], since sinPHI>0 - easy to check
-    // TVector3 hadr_plane_par = (y.Cross(scat_plane_norm)).Cross(y);
-    // this definition is used instead, which allows negative sinPHI!
-    TVector3 hadr_plane_par = z.Cross(y);
-    hadr_plane_par = hadr_plane_par.Unit();
-
-    Double_t    cosPHI = scat_plane_norm.Dot(y);
-    Double_t    sinPHI = scat_plane_norm.Dot(hadr_plane_par);
-
-    Double_t    PHI = atan2(sinPHI, cosPHI);
-
-    if (PHI<0) PHI += 2 * TMath::Pi();
-
-    // sanity check
-    Double_t diff = TMath::Abs(PHI_h - PHI);
-    if (diff>1e-5) {
-        cout << "ERROR: PHI determined by different methods differ by " << diff << endl;
-        abort();
-    }
-
-    // done
-    return PHI_h;
-}
-
-TLorentzVector TMiniNtupleAnalyzer::getPiPlusInRhoHelicityFrame(TLorentzVector pi_plus, TLorentzVector pi_minus, bool q_reco) {
-
-    // get boost and rotation vectors as well as the gamma* vector
-    get_gammaP_boost(q_reco, true);
-
-    TLorentzVector rho = pi_plus + pi_minus;
-    rho.Boost(fBoost);
-    rho.Rotate(fAngle, fRotationAxis);
-
-    pi_plus.Boost(fBoost);
-    pi_plus.Rotate(fAngle, fRotationAxis);
-
-    // rotate the system in such a way that rho direction is Z 
-    // i.e. the outgoing proton direction is opposite to Z - according to a definition of the helicity frame)
-    TVector3 z(0,0,1);
-    TVector3 rho_vect = rho.Vect();
-    // rotation angle is the angle between the original z axis and the rho direction
-    // acos input: [-1, 1] output: [0, pi]
-    Double_t rotation_angle = acos(rho.Pz()/rho.P());
-    // rotation axis is a vector perpendicular to both rho direction and the z axis (i.e. z axis is rotated in z-rho plane)
-    TVector3 rotation_axis = rho_vect.Cross(z);
-
-    // perform rotation of lab vectors (need rho and positive pion)
-    rho.Rotate(rotation_angle, rotation_axis);
-    pi_plus.Rotate(rotation_angle, rotation_axis);
-
-    // determine the boost vector, which is opposite to rho velocity vector and is given in terms of c
-    // (for any lorentz vector, velocity/c = momentum*c/energy, we work in natural units, c=1)
-    Double_t px = rho.Px();
-    Double_t py = rho.Py();
-    Double_t pz = rho.Pz();
-    Double_t e  = rho.E();
-    TVector3 boost(-px/e, -py/e, -pz/e);
-
-    // finally, boost the vectors to the rho rest frame
-    rho.Boost(boost);
-    pi_plus.Boost(boost);
-
-    // return rotated and boosted pi+
-    return pi_plus;
-}
-
-Double_t TMiniNtupleAnalyzer::get_cos_theta_h(TLorentzVector pi_plus, TLorentzVector pi_minus, bool q2_reco) {
-
-    // get p+ in rho helicity frame
-    TLorentzVector pi_plus_rho_frame = getPiPlusInRhoHelicityFrame(pi_plus, pi_minus, q2_reco);
-
-    // theta*) is the angle between the pi+ direction and the z direction in this frame (e.g. Theta)
-    Double_t cos_theta_h = pi_plus_rho_frame.Z() / pi_plus_rho_frame.P();
-
-    return cos_theta_h;
-}
-
-Double_t TMiniNtupleAnalyzer::get_phi_h(TLorentzVector pi_plus, TLorentzVector pi_minus, bool q2_reco) {
-
-    // get p+ in rho helicity frame
-    TLorentzVector pi_plus_rho_frame = getPiPlusInRhoHelicityFrame(pi_plus, pi_minus, q2_reco);
-
-    // Phi_h is azimutal angle of pi+ in this frame
-    Double_t phi_h = pi_plus_rho_frame.Phi();
-    phi_h = TVector2::Phi_0_2pi(phi_h);
-
-    return phi_h;
-}
-
-Double_t TMiniNtupleAnalyzer::get_cos_theta_star(TLorentzVector pi_plus, TLorentzVector pi_minus) {
-
-    // construct the rho
-    TLorentzVector rho = pi_plus + pi_minus;
-
-    // rotate the system in such a way that rho direction is Z 
-    // i.e. the outgoing proton direction is opposite to Z - according to a definition of the helicity frame)
-    TVector3 z(0,0,1);
-    TVector3 rho_vect = rho.Vect();
-    // rotation angle is the angle between the original z axis and the rho direction
-    // acos input: [-1, 1] output: [0, pi]
-    Double_t rotation_angle = acos(rho.Pz()/rho.P());
-    // rotation axis is a vector perpendicular to both rho direction and the z axis (i.e. z axis is rotated in z-rho plane)
-    TVector3 rotation_axis = rho_vect.Cross(z);
-
-    // perform rotation of lab vectors (need rho and positive pion)
-    rho.Rotate(rotation_angle, rotation_axis);
-    pi_plus.Rotate(rotation_angle, rotation_axis);
-
-    // determine the boost vector, which is opposite to rho velocity vector and is given in terms of c
-    // (for any lorentz vector, velocity/c = momentum*c/energy, we work in natural units, c=1)
-    Double_t px = rho.Px();
-    Double_t py = rho.Py();
-    Double_t pz = rho.Pz();
-    Double_t e  = rho.E();
-    TVector3 boost(-px/e, -py/e, -pz/e);
-
-    // finally, boost the vectors to the rho rest frame
-    rho.Boost(boost);
-    pi_plus.Boost(boost);
-
-    // get the cos theta star
-    Double_t cos_theta_star = pi_plus.Z()/pi_plus.P();
-
-    // done
-    return cos_theta_star;
-}
-
 void TMiniNtupleAnalyzer::TrackingEfficiency() {
 
     // get a pointer to inclusive bin
@@ -1259,6 +1021,244 @@ Double_t TMiniNtupleAnalyzer::getIslandDCA(int isl, int trk) {
     // calculate DCA
     Double_t    DCA = sqrt(1-cos_alpha*cos_alpha) * island_relative.Mag();
     return  DCA;
+}
+
+Double_t TMiniNtupleAnalyzer::getElectronEnergyConstrainedMethod(TLorentzVector rho) {
+
+    // use rho information to calculate scattered electron enegry  - "constrained" method, see e.g. eur phys c 6, 603-627 (1999)
+
+    // calculate e-pZ of the rho
+    Double_t    empz = rho.E() - rho.Pz();
+
+    // calculate electron energy
+    Double_t    E_elec = (2*E_BEAM - empz)/(1 - cos(Sith[0]));
+
+    // done
+    return E_elec;
+}
+
+TLorentzVector TMiniNtupleAnalyzer::getElectronConstrainedMethod(TLorentzVector rho) {
+
+    // get electron energy from constrained method
+    Double_t    E_elec = getElectronEnergyConstrainedMethod(rho);
+
+    // set scattered electron parameters
+    TLorentzVector electron; 
+    electron.SetXYZM(E_elec*sin(Sith[0])*cos(Siph[0]), E_elec*sin(Sith[0])*sin(Siph[0]), E_elec*cos(Sith[0]), M_ELECTRON);
+
+    return electron;
+}
+
+Double_t TMiniNtupleAnalyzer::getQ2ConstrainedMethod(TLorentzVector rho) {
+
+    // get electron energy from constrained method
+    Double_t    E_elec = getElectronEnergyConstrainedMethod(rho);
+
+    // calculate Q2
+    Double_t    q2 = 2 * E_BEAM * E_elec * ( 1 + cos(Sith[0]) );
+
+    // done
+    return q2;
+}
+
+Double_t TMiniNtupleAnalyzer::get_PHI_h(TLorentzVector rho, bool q_reco) {
+
+    // set incoming/outgoing lepton 4vectors
+    TLorentzVector  e_incoming(0, 0, (-1)*E_BEAM, sqrt(E_BEAM*E_BEAM + M_ELECTRON * M_ELECTRON));
+    TLorentzVector  e_outgoing;
+    if (q_reco) {
+        e_outgoing = getElectronConstrainedMethod(rho);
+    } else {
+        e_outgoing.SetXYZM(Plepton[0], Plepton[1], Plepton[2], M_ELECTRON);
+    }
+
+    // set the incoming proton 4-vector
+    TLorentzVector p_incoming(0, 0, E_PROTON, sqrt( E_PROTON * E_PROTON + M_PROTON * M_PROTON ) );
+
+    // get boost and rotation vectors as well as the gamma* vector
+    get_gammaP_boost(q_reco, true);
+
+    // boost leptons, incoming proton and the rho to the gamma*-p frame
+    e_incoming.Boost(fBoost);
+    e_incoming.Rotate(fAngle, fRotationAxis);
+
+    e_outgoing.Boost(fBoost);
+    e_outgoing.Rotate(fAngle, fRotationAxis);
+
+    p_incoming.Boost(fBoost);
+    p_incoming.Rotate(fAngle, fRotationAxis);
+
+    rho.Boost(fBoost);
+    rho.Rotate(fAngle, fRotationAxis);
+
+    // get normals to the scattering and production planes
+    TVector3 e_incoming_3vect = e_incoming.Vect();
+    TVector3 e_outgoing_3vect = e_outgoing.Vect();
+    TVector3 scat_plane_perp = e_incoming_3vect.Cross(e_outgoing_3vect);
+
+    TVector3 gamma_3vect = fq.Vect();
+    TVector3 rho_3vect = rho.Vect();
+    TVector3 prod_plane_perp = gamma_3vect.Cross(rho_3vect);
+
+    // these vectors are perpendicular to the Z axis (incoming proton/gamma* direction),
+    // hence can work in 2D space - that is in XY plane of the gamma*-p frame
+    TVector2 vscat = scat_plane_perp.XYvector();
+    TVector2 vprod = prod_plane_perp.XYvector();
+
+    // rotate both vectors in such a way that vscat has phi = 0
+    vprod=vprod.Rotate((-1)*vscat.Phi());
+    vscat=vscat.Rotate((-1)*vscat.Phi());
+
+    // the PHI_h is just the phi angle of vprod (range [0, 2pi], see ROOT docs)
+    Double_t PHI_h=vprod.Phi();
+
+    // sanity check
+    if ( TMath::IsNaN(PHI_h) ) {
+        cout << "ERROR: helicity angle is NAN" << endl;
+        abort();
+    }
+
+    // the other - equivalent way to define PHI:
+    // very similar to the definition in NUCL PHYS B 61 381-413 (1973),
+    // except a small thing, see below
+
+    TVector3 z = fq.Vect();
+    z = z.Unit();
+
+    TVector3 y = z.Cross(rho_3vect);
+    y = y.Unit();
+
+    TVector3 x = y.Cross(z);
+
+    TVector3 scat_plane_norm = e_incoming_3vect.Cross(e_outgoing_3vect);
+    scat_plane_norm = scat_plane_norm.Unit();
+
+    // the commented line is according to the paper; however
+    // in this case PHI is [0, pi] and not [0, 2pi], since sinPHI>0 - easy to check
+    // TVector3 hadr_plane_par = (y.Cross(scat_plane_norm)).Cross(y);
+    // this definition is used instead, which allows negative sinPHI!
+    TVector3 hadr_plane_par = z.Cross(y);
+    hadr_plane_par = hadr_plane_par.Unit();
+
+    Double_t    cosPHI = scat_plane_norm.Dot(y);
+    Double_t    sinPHI = scat_plane_norm.Dot(hadr_plane_par);
+
+    Double_t    PHI = atan2(sinPHI, cosPHI);
+
+    if (PHI<0) PHI += 2 * TMath::Pi();
+
+    // sanity check
+    Double_t diff = TMath::Abs(PHI_h - PHI);
+    if (diff>1e-5) {
+        cout << "ERROR: PHI determined by different methods differ by " << diff << endl;
+        abort();
+    }
+
+    // done
+    return PHI_h;
+}
+
+TLorentzVector TMiniNtupleAnalyzer::getPiPlusInRhoHelicityFrame(TLorentzVector pi_plus, TLorentzVector pi_minus, bool q_reco) {
+
+    // get boost and rotation vectors as well as the gamma* vector
+    get_gammaP_boost(q_reco, true);
+
+    TLorentzVector rho = pi_plus + pi_minus;
+    rho.Boost(fBoost);
+    rho.Rotate(fAngle, fRotationAxis);
+
+    pi_plus.Boost(fBoost);
+    pi_plus.Rotate(fAngle, fRotationAxis);
+
+    // rotate the system in such a way that rho direction is Z 
+    // i.e. the outgoing proton direction is opposite to Z - according to a definition of the helicity frame)
+    TVector3 z(0,0,1);
+    TVector3 rho_vect = rho.Vect();
+    // rotation angle is the angle between the original z axis and the rho direction
+    // acos input: [-1, 1] output: [0, pi]
+    Double_t rotation_angle = acos(rho.Pz()/rho.P());
+    // rotation axis is a vector perpendicular to both rho direction and the z axis (i.e. z axis is rotated in z-rho plane)
+    TVector3 rotation_axis = rho_vect.Cross(z);
+
+    // perform rotation of lab vectors (need rho and positive pion)
+    rho.Rotate(rotation_angle, rotation_axis);
+    pi_plus.Rotate(rotation_angle, rotation_axis);
+
+    // determine the boost vector, which is opposite to rho velocity vector and is given in terms of c
+    // (for any lorentz vector, velocity/c = momentum*c/energy, we work in natural units, c=1)
+    Double_t px = rho.Px();
+    Double_t py = rho.Py();
+    Double_t pz = rho.Pz();
+    Double_t e  = rho.E();
+    TVector3 boost(-px/e, -py/e, -pz/e);
+
+    // finally, boost the vectors to the rho rest frame
+    rho.Boost(boost);
+    pi_plus.Boost(boost);
+
+    // return rotated and boosted pi+
+    return pi_plus;
+}
+
+Double_t TMiniNtupleAnalyzer::get_cos_theta_h(TLorentzVector pi_plus, TLorentzVector pi_minus, bool q2_reco) {
+
+    // get p+ in rho helicity frame
+    TLorentzVector pi_plus_rho_frame = getPiPlusInRhoHelicityFrame(pi_plus, pi_minus, q2_reco);
+
+    // theta*) is the angle between the pi+ direction and the z direction in this frame (e.g. Theta)
+    Double_t cos_theta_h = pi_plus_rho_frame.Z() / pi_plus_rho_frame.P();
+
+    return cos_theta_h;
+}
+
+Double_t TMiniNtupleAnalyzer::get_phi_h(TLorentzVector pi_plus, TLorentzVector pi_minus, bool q2_reco) {
+
+    // get p+ in rho helicity frame
+    TLorentzVector pi_plus_rho_frame = getPiPlusInRhoHelicityFrame(pi_plus, pi_minus, q2_reco);
+
+    // Phi_h is azimutal angle of pi+ in this frame
+    Double_t phi_h = pi_plus_rho_frame.Phi();
+    phi_h = TVector2::Phi_0_2pi(phi_h);
+
+    return phi_h;
+}
+
+Double_t TMiniNtupleAnalyzer::get_cos_theta_star(TLorentzVector pi_plus, TLorentzVector pi_minus) {
+
+    // construct the rho
+    TLorentzVector rho = pi_plus + pi_minus;
+
+    // rotate the system in such a way that rho direction is Z 
+    // i.e. the outgoing proton direction is opposite to Z - according to a definition of the helicity frame)
+    TVector3 z(0,0,1);
+    TVector3 rho_vect = rho.Vect();
+    // rotation angle is the angle between the original z axis and the rho direction
+    // acos input: [-1, 1] output: [0, pi]
+    Double_t rotation_angle = acos(rho.Pz()/rho.P());
+    // rotation axis is a vector perpendicular to both rho direction and the z axis (i.e. z axis is rotated in z-rho plane)
+    TVector3 rotation_axis = rho_vect.Cross(z);
+
+    // perform rotation of lab vectors (need rho and positive pion)
+    rho.Rotate(rotation_angle, rotation_axis);
+    pi_plus.Rotate(rotation_angle, rotation_axis);
+
+    // determine the boost vector, which is opposite to rho velocity vector and is given in terms of c
+    // (for any lorentz vector, velocity/c = momentum*c/energy, we work in natural units, c=1)
+    Double_t px = rho.Px();
+    Double_t py = rho.Py();
+    Double_t pz = rho.Pz();
+    Double_t e  = rho.E();
+    TVector3 boost(-px/e, -py/e, -pz/e);
+
+    // finally, boost the vectors to the rho rest frame
+    rho.Boost(boost);
+    pi_plus.Boost(boost);
+
+    // get the cos theta star
+    Double_t cos_theta_star = pi_plus.Z()/pi_plus.P();
+
+    // done
+    return cos_theta_star;
 }
 
 Double_t TMiniNtupleAnalyzer::getRhoPhiWeight(Double_t phi) {
