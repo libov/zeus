058722c41a56531b4c5d58948e4d6a32086d26a7
Vladyslav <libov@mail.desy.de>
2012-12-04 11:10:02 +0100

    code  format

--- NtupleAnalyzer/inc/TGlobalBin.h
+++ NtupleAnalyzer/inc/TGlobalBin.h
@@ -26,40 +26,40 @@ class TMiniNtupleAnalyzer;
 */
 
 class TGlobalBin : public TObject {
-	public:
 
-		TGlobalBin() {};
-		TGlobalBin(TMiniNtupleAnalyzer *instance);
-		~TGlobalBin(){};
+    public:
 
-		void							AddBin(TBin * bin);
-		Bool_t						CheckGlobalBin(VariablePhase	VarPhase);
-		TString						BinName;
-		TString						GetBinDescription() {return fBinDescription;}
-		void							AddHistogram(TH1F * histogram) {fListHistograms->Add(histogram);}
-		int							FillHistogram(TString HistTitle, Float_t	Value);
-		TList*						GetHistList() {return fListHistograms;}
-		void							AddMirroredHistTitle(TString title) {fMirroredHistTitle.push_back(title);}
-		void							MirrorHistograms();
-		void							SetWeightingFactor(Double_t		factor) {fWeightingFactor = factor;}
-		Double_t						GetWeightingFactor() {return fWeightingFactor;}
-                void        Print();
+        TGlobalBin() {};
+        TGlobalBin(TMiniNtupleAnalyzer *instance);
+        ~TGlobalBin(){};
 
-	private:
+        void        AddBin(TBin * bin);
+        Bool_t      CheckGlobalBin(VariablePhase VarPhase);
+        TString     BinName;
+        TString     GetBinDescription() { return fBinDescription; }
+        void        AddHistogram(TH1F * histogram) { fListHistograms->Add(histogram); }
+        int         FillHistogram(TString HistTitle, Float_t Value);
+        TList*      GetHistList() { return fListHistograms; }
+        void        AddMirroredHistTitle(TString title) { fMirroredHistTitle.push_back(title); }
+        void        MirrorHistograms();
+        void        SetWeightingFactor(Double_t factor) { fWeightingFactor = factor; }
+        Double_t    GetWeightingFactor() { return fWeightingFactor; }
+        void        Print();
 
-		TList*						fList_TBin;
-		TString						fBinDescription;
-		TList*						fListHistograms;
-		TMiniNtupleAnalyzer		*fAnalyzerInstance;		// pointer to current TMiniNtupleAnalyzer
-																		// object; used for access to some variables
-																		// e.g. for histogram mirroring
-																		// this mechanism could be used also for
-																		// checking of bin values actually...
-		vector<TString>			fMirroredHistTitle;
-		Int_t							fNevents;					// number of events that fall into this bin
-																		//
-		Double_t						fWeightingFactor;			//!< Weighting factor for histograms filling.
-																		//!< Is used to scale charm distribution when adding up
-																		//!< samples with different q2-min-cut
+    private:
+
+        TList*      fList_TBin;
+        TString     fBinDescription;
+        TList*      fListHistograms;
+        TMiniNtupleAnalyzer *fAnalyzerInstance; // pointer to current TMiniNtupleAnalyzer
+                                                // object; used for access to some variables
+                                                // e.g. for histogram mirroring
+                                                // this mechanism could be used also for
+                                                // checking of bin values actually...
+        vector<TString> fMirroredHistTitle;
+        Int_t           fNevents;               // number of events that fall into this bin
+        Double_t        fWeightingFactor;       //!< Weighting factor for histograms filling.
+                                                //!< Is used to scale charm distribution when adding up
+                                                //!< samples with different q2-min-cut
 };
 #endif
