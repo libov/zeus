45f1a036bff9a8a395a75efcf76b7329de141760
Vladyslav <libov@mail.desy.de>
2013-01-29 16:45:23 +0100

    added a script to run correct_qed program

---
+++ TXMLparser/correct_qed.sh
@@ -0,0 +1,3 @@
+./correct_qed --XMLfile results.full.forCHARM2.52.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_charm_correction_2.74
+./correct_qed --XMLfile results.full2.53.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_beauty_correction_2.75 --beauty
+
