1e4d4a55f8ea41fed74afeab4a476fa755ace2d2
Vladyslav <libov@mail.desy.de>
2013-02-04 09:33:05 +0100

    steering script update

--- TXMLparser/paper_f2.sh
+++ TXMLparser/paper_f2.sh
@@ -1,3 +1,3 @@
-./f2bc --meta_file meta_Jun21_2012_09:45:50 --XMLfile results.full.forCHARM2.52.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_charm_2.46.SYST
+./f2bc --meta_file meta_Jun21_2012_09:45:50 --XMLfile results.full.forCHARM2.52.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_charm_2.46.SYST --no_qed
 
-./f2bc --meta_file meta_Jun21_2012_09:54:42 --XMLfile results.full2.53.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_beauty_2.47.SYST --beauty
+./f2bc --meta_file meta_Jun21_2012_09:54:42 --XMLfile results.full2.53.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_beauty_2.47.SYST --beauty --no_qed
