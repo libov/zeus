7d7a6a29deb266d06880dabfe36638fac195541c
Vladyslav <libov@mail.desy.de>
2013-03-22 11:42:18 +0100

    changed 2.74/2.75 to 2.77/2.78 in QED correction script

--- TXMLparser/correct_qed.sh
+++ TXMLparser/correct_qed.sh
@@ -1,3 +1,2 @@
-./correct_qed --XMLfile results.full.forCHARM2.52.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_charm_correction_2.74
-./correct_qed --XMLfile results.full2.53.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_beauty_correction_2.75 --beauty
-
+./correct_qed --XMLfile results.full.forCHARM2.52.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_charm_correction_2.77
+./correct_qed --XMLfile results.full2.53.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_beauty_correction_2.78 --beauty
