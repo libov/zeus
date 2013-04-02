a55f1481d4d43532d17cea455548f10ab9dd569f
Vladyslav <libov@mail.desy.de>
2013-04-02 15:25:11 +0200

    updated tracking efficiency systematics

--- other/total_systematics/total_cross_section_syst.cxx
+++ other/total_systematics/total_cross_section_syst.cxx
@@ -11,11 +11,11 @@ Float_t total(Float_t * array, unsigned n) {
 
 int total_cross_section_syst() {
 
-    Float_t b_plus[]={1.4, 2, 2.8, 1.3, 0.8, 0.7, 3, 1.7, 1.8, 2};
-    Float_t b_minus[]={1.4, 2.8, 1.3, 0.8, 0.7, 3, 1.3, 0.9, 3.1, 2.1, 2};
+    Float_t b_plus[]={1.4, 2, 2.0, 1.3, 0.8, 0.7, 3, 1.7, 1.8, 2};
+    Float_t b_minus[]={1.4, 2.0, 1.3, 0.8, 0.7, 3, 1.3, 0.9, 3.1, 2.1, 2};
 
-    Float_t c_plus[]={0.8, 1, 0.9, 1.2, 0.8, 0.9, 2, 2.2, 1.5, 2, 1.0, 3.5, 2};
-    Float_t c_minus[]={0.8, 0.9, 1.2, 0.8, 0.9, 2, 1.7, 1.0, 2, 2.6, 2};
+    Float_t c_plus[]={0.8, 1, 0.5, 1.2, 0.8, 0.9, 2, 2.2, 1.5, 2, 1.0, 3.5, 2};
+    Float_t c_minus[]={0.8, 0.5, 1.2, 0.8, 0.9, 2, 1.7, 1.0, 2, 2.6, 2};
 
     cout << "beauty +" << total (b_plus, 10) << " -" << total (b_minus, 11) << endl;
     cout << "charm +" << total (c_plus, 13) << " -" << total (c_minus, 11) << endl;
