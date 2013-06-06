3a4b1c97cbdb4b794a89ea70dc0cd01b95bf45d5
Vladyslav <libov@mail.desy.de>
2013-06-06 12:55:55 +0200

    code cleanup

--- TXMLparser/inc/TXMLparser.h
+++ TXMLparser/inc/TXMLparser.h
@@ -26,18 +26,17 @@ class TXMLparser {
 
         TXMLparser(){};
         TXMLparser(TString xml_filename);
-        ~TXMLparser();
+        ~TXMLparser(){};
 
         void            WriteXMLfile(TString file_name);
-        void            setVerbose(bool is_verbose) { fIsVerbose = is_verbose;} // NOTE: useless at the moment (is used during constructor of the object) hence relies just on the default value
 
         DOMDocument*    getDOMDocument() { return fXMLdocument; }
 
     protected:
 
         void            OpenFile();     //!< is called from constructor
+
         TString         fXMLfileName;   //!< name of the XML file
-        bool            fIsVerbose;     //!< a flag saying whether messages should be printed or not
 
         //@{
         /** underlying Xerces objects to store the structure from an XML file */
--- TXMLparser/src/TXMLparser.cxx
+++ TXMLparser/src/TXMLparser.cxx
@@ -2,7 +2,7 @@
 //                                              //
 //  Inclusive  secondary vertex analysis        //
 //  Libov Vladyslav                             //
-//  DESY			                                 //
+//  DESY                                        //
 //  libov@mail.desy.de                          //
 //  February 2011                               //
 //                                              //
@@ -31,83 +31,82 @@
 using namespace std;
 using namespace xercesc;
 
-TXMLparser::TXMLparser(TString	xml_filename):
-fXMLfileName(xml_filename),
-fIsVerbose(false)
- {
-	// initialize xerces
-	try {
-		XMLPlatformUtils::Initialize();
-	} catch (const XMLException& toCatch) {
-		char* message = XMLString::transcode(toCatch.getMessage());
-		cout << "Error during initialization! :\n"<< message << "\n";
-		XMLString::release(&message);
-		abort();
-	}
+TXMLparser::TXMLparser(TString xml_filename):
+fXMLfileName(xml_filename)
+{
 
-	// initialize xerces DOM parser
-	fXMLparser = new XercesDOMParser();
+    // initialize xerces
+    try {
+        XMLPlatformUtils::Initialize();
+    } catch (const XMLException& toCatch) {
+        char* message = XMLString::transcode(toCatch.getMessage());
+        cout << "Error during initialization! :\n"<< message << "\n";
+        XMLString::release(&message);
+        abort();
+    }
 
-	fXMLparser->setValidationScheme(XercesDOMParser::Val_Always);
-	fXMLparser->setDoNamespaces(true);    // optional
+    // initialize xerces DOM parser
+    fXMLparser = new XercesDOMParser();
 
-	ErrorHandler* errHandler = (ErrorHandler*) new HandlerBase();
-	fXMLparser->setErrorHandler(errHandler);
+    fXMLparser->setValidationScheme(XercesDOMParser::Val_Always);
+    fXMLparser->setDoNamespaces(true);    // optional
 
-        // now open the file
-        OpenFile();
+    ErrorHandler* errHandler = (ErrorHandler*) new HandlerBase();
+    fXMLparser->setErrorHandler(errHandler);
+
+    // now open the file
+    OpenFile();
 }
 
 void    TXMLparser::OpenFile() {
-	// get path to the XML file
-	TString	XMLfilePath = getenv("DATABASE_PATH");
-	XMLfilePath += "/" + fXMLfileName;
-        
-	// parse the XML file!
-        if (fIsVerbose) cout << "INFO: Opening " <<XMLfilePath << endl;
-	try {
-		fXMLparser->parse(XMLfilePath.Data());
-	} catch (const XMLException& toCatch) {
-		char* message = XMLString::transcode(toCatch.getMessage());
-		cout << "Exception message is: \n" << message << "\n";
-		XMLString::release(&message);
-		abort();
-	} catch (const DOMException& toCatch) {
-		char* message = XMLString::transcode(toCatch.msg);
-		cout << "Exception message is: \n" << message << "\n";
-		XMLString::release(&message);
-		abort();
-	} catch (...) {
-		cout << "ERROR: Cannot open file: " << XMLfilePath << "\n Unexpected Exception... \n" ;
-		abort();
-	}
 
-        // get current XML document
-	fXMLdocument = fXMLparser->getDocument();
-}
+    // get path to the XML file
+    TString XMLfilePath = getenv("DATABASE_PATH");
+    XMLfilePath += "/" + fXMLfileName;
 
-TXMLparser::~TXMLparser() {
+    // parse the XML file!
+    cout << "INFO: Opening " <<XMLfilePath << endl;
+    try {
+        fXMLparser->parse(XMLfilePath.Data());
+    } catch (const XMLException& toCatch) {
+        char* message = XMLString::transcode(toCatch.getMessage());
+        cout << "Exception message is: \n" << message << "\n";
+        XMLString::release(&message);
+        abort();
+    } catch (const DOMException& toCatch) {
+        char* message = XMLString::transcode(toCatch.msg);
+        cout << "Exception message is: \n" << message << "\n";
+        XMLString::release(&message);
+        abort();
+    } catch (...) {
+        cout << "ERROR: Cannot open file: " << XMLfilePath << "\n Unexpected Exception... \n" ;
+        abort();
+    }
+
+    // get current XML document
+    fXMLdocument = fXMLparser -> getDocument();
 }
 
 void TXMLparser::WriteXMLfile(TString file_name) {
-        // DOMImplementationLS contains factory methods for creating objects
-        // that implement the DOMBuilder and the DOMWriter interfaces
-        static const XMLCh gLS[] = { chLatin_L, chLatin_S, chNull };
-        DOMImplementation *impl = DOMImplementationRegistry::getDOMImplementation(gLS);
 
-        // construct the DOMBuilder
-        DOMBuilder* myParser = ((DOMImplementationLS*)impl) -> createDOMBuilder(DOMImplementationLS::MODE_SYNCHRONOUS, 0);
+    // DOMImplementationLS contains factory methods for creating objects
+    // that implement the DOMBuilder and the DOMWriter interfaces
+    static const XMLCh gLS[] = { chLatin_L, chLatin_S, chNull };
+    DOMImplementation *impl = DOMImplementationRegistry::getDOMImplementation(gLS);
 
-        // construct the DOMWriter
-        DOMWriter* myWriter = ((DOMImplementationLS*)impl)->createDOMWriter();
+    // construct the DOMBuilder
+    DOMBuilder* myParser = ((DOMImplementationLS*)impl) -> createDOMBuilder(DOMImplementationLS::MODE_SYNCHRONOUS, 0);
 
-        XMLFormatTarget *myFormTarget;
-        // get path to the XML file
-	TString	XMLfilePath = getenv("DATABASE_PATH");
-	XMLfilePath += "/" + file_name;
-        // set it
-        myFormTarget = new LocalFileFormatTarget(XMLfilePath.Data());
-        // write to file!
-        cout << "Writing data to " << XMLfilePath << endl;
-        myWriter -> writeNode(myFormTarget, *fXMLdocument);
+    // construct the DOMWriter
+    DOMWriter* myWriter = ((DOMImplementationLS*)impl)->createDOMWriter();
+
+    XMLFormatTarget *myFormTarget;
+    // get path to the XML file
+    TString	XMLfilePath = getenv("DATABASE_PATH");
+    XMLfilePath += "/" + file_name;
+    // set it
+    myFormTarget = new LocalFileFormatTarget(XMLfilePath.Data());
+    // write to file!
+    cout << "INFO: Writing data to " << XMLfilePath << endl;
+    myWriter -> writeNode(myFormTarget, *fXMLdocument);
 }
