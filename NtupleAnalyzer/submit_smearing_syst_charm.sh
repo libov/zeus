9652b5a6896d90572e4de8410de9247a0501c1de
Vladyslav <libov@mail.desy.de>
2013-06-28 17:45:24 +0200

    added a script for smearing systematic check

---
+++ NtupleAnalyzer/submit_smearing_syst_charm.sh
@@ -0,0 +1,21 @@
+
+# core variation
+./submit_analysis -b full.forCHARM -v 2.88.2 -d --gaus1prob 0.03 --gaus2prob 0.002
+./submit_analysis -b full.forCHARM -v 2.88.3 -d --gaus1prob 0.05 --gaus2prob 0.002
+./submit_analysis -b full.forCHARM -v 2.88.4 -d --gaus1prob 0.06 --gaus2prob 0.002
+./submit_analysis -b full.forCHARM -v 2.88.5 -d --gaus1prob 0.07 --gaus2prob 0.002
+./submit_analysis -b full.forCHARM -v 2.88.6 -d --gaus1prob 0.08 --gaus2prob 0.002
+./submit_analysis -b full.forCHARM -v 2.88.7 -d --gaus1prob 0.09 --gaus2prob 0.002
+./submit_analysis -b full.forCHARM -v 2.88.8 -d --gaus1prob 0.11 --gaus2prob 0.002
+
+# tail variation
+./submit_analysis -b full.forCHARM -v 2.88.9  -d --gaus1prob 0.07 --gaus2prob 0.000
+./submit_analysis -b full.forCHARM -v 2.88.10 -d --gaus1prob 0.07 --gaus2prob 0.0005
+./submit_analysis -b full.forCHARM -v 2.88.11 -d --gaus1prob 0.07 --gaus2prob 0.001
+./submit_analysis -b full.forCHARM -v 2.88.12 -d --gaus1prob 0.07 --gaus2prob 0.0015
+./submit_analysis -b full.forCHARM -v 2.88.13 -d --gaus1prob 0.07 --gaus2prob 0.002
+./submit_analysis -b full.forCHARM -v 2.88.14 -d --gaus1prob 0.07 --gaus2prob 0.0025
+./submit_analysis -b full.forCHARM -v 2.88.15 -d --gaus1prob 0.07 --gaus2prob 0.003
+./submit_analysis -b full.forCHARM -v 2.88.16 -d --gaus1prob 0.07 --gaus2prob 0.004
+./submit_analysis -b full.forCHARM -v 2.88.17 -d --gaus1prob 0.07 --gaus2prob 0.005
+
