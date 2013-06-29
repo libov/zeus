90aea98536f8208ce1fad813165e145980dc84a4
Vladyslav <libov@mail.desy.de>
2013-06-29 16:52:16 +0200

    added analysis submit scripts for tracking systematics

---
+++ NtupleAnalyzer/submit_tracking_syst_beauty.sh
@@ -0,0 +1,7 @@
+submit_analysis -b full -v 2.78.1 --tracking_correction 0
+submit_analysis -b full -v 2.78.2 --tracking_correction 0.2
+submit_analysis -b full -v 2.78.3 --tracking_correction 0.4
+submit_analysis -b full -v 2.78.4 --tracking_correction 0.6
+submit_analysis -b full -v 2.78.5 --tracking_correction 0.8
+submit_analysis -b full -v 2.78.6 --tracking_correction 1.0
+	
---
+++ NtupleAnalyzer/submit_tracking_syst_charm.sh
@@ -0,0 +1,7 @@
+submit_analysis -b full.forCHARM -v 2.77.1 --tracking_correction 0
+submit_analysis -b full.forCHARM -v 2.77.2 --tracking_correction 0.2
+submit_analysis -b full.forCHARM -v 2.77.3 --tracking_correction 0.4
+submit_analysis -b full.forCHARM -v 2.77.4 --tracking_correction 0.6
+submit_analysis -b full.forCHARM -v 2.77.5 --tracking_correction 0.8
+submit_analysis -b full.forCHARM -v 2.77.6 --tracking_correction 1.0
+	
