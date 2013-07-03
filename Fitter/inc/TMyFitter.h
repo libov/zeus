76f3e5c8a141bd1d5764829e3d31f68eb6c6398d
Vladyslav <libov@mail.desy.de>
2013-07-03 11:38:09 +0200

    code indentation

--- Fitter/inc/TMyFitter.h
+++ Fitter/inc/TMyFitter.h
@@ -13,61 +13,61 @@
 #include<TMinuit.h>
 #include <inc/TDistribution.h>
 
-class	TMyFitter : public TObject
+class TMyFitter : public TObject
 {
 
-	public:
-		TMyFitter() {};
-		TMyFitter(TString VariableName, void (*func)(Int_t&, Double_t*, Double_t&, Double_t*, Int_t), TString HistogramsVersion);
-		TMyFitter(TString VariableName, TString HistogramsVersion);
-		~TMyFitter() {};
+    public:
 
-		Double_t	GetChi2(Double_t p1, Double_t p2, Double_t p3);
-		Double_t	GetMinChi2(){return fChi2Min;};
+        TMyFitter() {};
+        TMyFitter(TString VariableName, void (*func)(Int_t&, Double_t*, Double_t&, Double_t*, Int_t), TString HistogramsVersion);
+        TMyFitter(TString VariableName, TString HistogramsVersion);
+        ~TMyFitter() {};
 
-		Int_t		Fit();
+        void        Initialize();
+        Int_t       Fit();
+        Double_t    GetChi2(Double_t p1, Double_t p2, Double_t p3);
+        Double_t    GetMinChi2() { return fChi2Min; }
 
-                void            SetScaleLF(bool set_scale) {fScaleLF = set_scale; };
-                void            SetScaleLFSize(Float_t scale_size) {fScaleLFSize = scale_size; };
+        // getters
+        Float_t     get_k_uds() {return k_uds;}
+        Float_t     get_k_c() {return k_c;}
+        Float_t     get_k_b() {return k_b;}
+        Float_t     get_k_uds_err() {return k_uds_err;}
+        Float_t     get_k_c_err() {return k_c_err;}
+        Float_t     get_k_b_err() {return k_b_err;}
 
-                // getters
-                Float_t         get_k_uds() {return k_uds;};
-                Float_t         get_k_c() {return k_c;};
-                Float_t         get_k_b() {return k_b;};
-                Float_t         get_k_uds_err() {return k_uds_err;};
-                Float_t         get_k_c_err() {return k_c_err;};
-                Float_t         get_k_b_err() {return k_b_err;};
+        void        SetScaleLF(bool set_scale) {fScaleLF = set_scale; }
+        void        SetScaleLFSize(Float_t scale_size) {fScaleLFSize = scale_size; }
 
-                void            Initialize();
+    private:
 
-	private:
-		Double_t	fChi2Min;
-		Double_t	fChi2init;
-		TString		fBinName;
-		TString		fHistogramsVersion;
+        Double_t    fChi2Min;
+        Double_t    fChi2init;
+        TString     fBinName;
+        TString     fHistogramsVersion;
 
-		//  Minuit object
-		TMinuit*	fMinuit;
+        //  Minuit object
+        TMinuit*    fMinuit;
 
-		/** histograms which are used for the fit
-		 */
-		TDistribution	bin1;
-		TDistribution	bin2;
-		TDistribution	bin3;
-		TDistribution	UnsubstrTOT;
+        /** histograms which are used for the fit
+         */
+        TDistribution    bin1;
+        TDistribution    bin2;
+        TDistribution    bin3;
+        TDistribution    UnsubstrTOT;
 
-		// scaling factors
-		Double_t	k_b;
-		Double_t	k_c;
-		Double_t	k_uds;
+        // scaling factors
+        Double_t    k_b;
+        Double_t    k_c;
+        Double_t    k_uds;
 
-		// scaling factors statistical uncertainties
-		Double_t	k_b_err;
-		Double_t	k_c_err;
-		Double_t	k_uds_err;
+        // scaling factors statistical uncertainties
+        Double_t    k_b_err;
+        Double_t    k_c_err;
+        Double_t    k_uds_err;
 
-                bool            fScaleLF;
-                Float_t         fScaleLFSize;
-
+        // parameters needed for LF asymmetry studies
+        bool        fScaleLF;
+        Float_t     fScaleLFSize;
 };
 
