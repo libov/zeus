16f7628ecd851c69c1258db83b9fcb7acee86dcc
Vladyslav <libov@mail.desy.de>
2013-09-10 16:33:34 +0200

    changed default path for the systematics files

--- other/total_systematics/total_systematics.py
+++ other/total_systematics/total_systematics.py
@@ -26,7 +26,7 @@ else:
     print  'INFO: beauty mode'
 
 # a directory with input files
-INPUT='/zow/user/libov/WWW/ZEUS_ONLY/F2cb_vtx_paper_May2012'
+INPUT='/zow/user/libov/WWW/ZEUS_ONLY/F2cb_vtx_paper_May2012/systematics'
 
 if BEAUTY:
     SYST_SOURCES=['BRSystematics_beauty', 'FragmFractionSystematics_beauty', 'tracking_map_beauty_systematics_2.78', 'charm_fragmentation_5.0', 'beauty_fragmentation_5.0', 'jet_energy_scale_beauty', 'smearing_core_beauty', 'smearing_tail_beauty', 'lf_asymmetry_beauty', 'et_reweighting_beauty', 'signal_extraction_beauty', 'DIS_y_beauty', 'DIS_Ee_beauty', 'DIS_empz_beauty', 'q2_reweighting_beauty', 'flt_efficiency_beauty', 'EMscale_beauty']
