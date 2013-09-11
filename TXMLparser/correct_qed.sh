bc7a55e9320856792553e3f12fac561e10e4bbc0
Vladyslav <libov@mail.desy.de>
2013-09-11 17:03:07 +0200

    updated final result version for the correct_qed script

--- TXMLparser/correct_qed.sh
+++ TXMLparser/correct_qed.sh
@@ -1,2 +1,2 @@
-./correct_qed --XMLfile results.full.forCHARM2.96.1.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_charm_correction_2.77
-./correct_qed --XMLfile results.full2.97.1.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_beauty_correction_2.78 --beauty
+./correct_qed --XMLfile results.full.forCHARM2.98.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_charm_correction_2.77
+./correct_qed --XMLfile results.full2.99.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_beauty_correction_2.78 --beauty
