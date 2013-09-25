0b666b532fcc985d282269bbcb23af69cb8bbc84
Vladyslav <libov@mail.desy.de>
2013-09-25 13:46:57 +0200

    changed ordering of systematics files - to make it consistent to that in the paper - but this changes nothing

--- TXMLparser/systematics.sh
+++ TXMLparser/systematics.sh
@@ -1,38 +1,57 @@
-./systematics --file beauty_fragmentation_4.2
-./systematics --file beauty_fragmentation_5.0
-./systematics --file charm_fragmentation_4.2
-./systematics --file charm_fragmentation_5.0
 ./systematics --file DIS_Ee_beauty
 ./systematics --file DIS_Ee_charm
-./systematics --file DIS_empz_beauty
-./systematics --file DIS_empz_charm
 ./systematics --file DIS_y_beauty
 ./systematics --file DIS_y_charm
-./systematics --file EMscale_beauty
-./systematics --file EMscale_charm
-./systematics --file eta_reweighting_charm
-./systematics --file eta_reweighting_beauty
-./systematics --file et_reweighting_beauty
-./systematics --file et_reweighting_charm
-./systematics --file etbeauty_reweighting_charm
-./systematics --file etbeauty_reweighting_beauty
-./systematics --file etabeauty_reweighting_charm
-./systematics --file etabeauty_reweighting_beauty
-./systematics --file jet_energy_scale_beauty
-./systematics --file jet_energy_scale_charm
-./systematics --file lf_asymmetry_beauty
-./systematics --file lf_asymmetry_charm
-./systematics --file signal_extraction_beauty
-./systematics --file signal_extraction_charm
-./systematics --file smearing_core_beauty
-./systematics --file smearing_core_charm
-./systematics --file smearing_tail_beauty
-./systematics --file smearing_tail_charm
+./systematics --file DIS_empz_beauty
+./systematics --file DIS_empz_charm
 
 export CN_VERSION=v06
 
 ./systematics --file tracking_map_charm_systematics_2.77
 ./systematics --file tracking_map_beauty_systematics_2.78
 
-export CN_VERSION=v02 
+export CN_VERSION=v02
+
+./systematics --file smearing_core_beauty
+./systematics --file smearing_core_charm
+./systematics --file smearing_tail_beauty
+./systematics --file smearing_tail_charm
+
+./systematics --file signal_extraction_beauty
+./systematics --file signal_extraction_charm
+
+./systematics --file jet_energy_scale_beauty
+./systematics --file jet_energy_scale_charm
+
+./systematics --file EMscale_beauty
+./systematics --file EMscale_charm
+
+./systematics --file eta_reweighting_beauty
+./systematics --file eta_reweighting_charm
+./systematics --file etabeauty_reweighting_beauty
+./systematics --file etabeauty_reweighting_charm
+./systematics --file et_reweighting_beauty
+./systematics --file et_reweighting_charm
+./systematics --file etbeauty_reweighting_beauty
+./systematics --file etbeauty_reweighting_charm
+
+./systematics --file lf_asymmetry_beauty
+./systematics --file lf_asymmetry_charm
+
+./systematics --file charm_fragmentation_5.0
+./systematics --file charm_fragmentation_4.2
+
+./systematics --file beauty_fragmentation_5.0
+./systematics --file beauty_fragmentation_4.2
+
+
+
+
+
+
+
+
+
+
+ 
 
