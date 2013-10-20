32971aa23c10e873244303b79fcefe7c645754e6
Vladyslav <libov@mail.desy.de>
2013-10-20 10:30:35 +0200

    changed analysis versions to latest

--- Fitter/print_results.sh
+++ Fitter/print_results.sh
@@ -1,5 +1,5 @@
 
-./result_printer --file results.full.forCHARM2.52.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_charm_correction_2.77QED_corrected.SYST   
+./result_printer --file results.full.forCHARM2.98.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_charm_correction_2.77QED_corrected.SYST   
 
-./result_printer --file results.full2.53.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_beauty_correction_2.78QED_corrected.SYST --beauty
+./result_printer --file results.full2.99.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_beauty_correction_2.78QED_corrected.SYST --beauty
 
