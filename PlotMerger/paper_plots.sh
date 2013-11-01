e773ed5c8d42b856701e710521703a08cbe2a638
Vladyslav <libov@mail.desy.de>
2013-11-01 11:02:05 +0100

    changed versions to the latest in the paper_plots.sh script

--- PlotMerger/paper_plots.sh
+++ PlotMerger/paper_plots.sh
@@ -1,7 +1,7 @@
-./plotter -b full.forCHARM -v 2.52.2 -d -s --config_file F2bc_paper_charm --no_indices
+./plotter -b full.forCHARM -v 2.98.2 -d -s --config_file F2bc_paper_charm --no_indices
 
-./plotter -b full -v 2.53.2 -d -s --config_file F2bc_paper_beauty --no_indices
+./plotter -b full -v 2.99.2 -d -s --config_file F2bc_paper_beauty --no_indices
 
-./plotter -b full.forCHARM -v 2.52.2 -d -s --config_file F2bc_paper_significance
+./plotter -b full.forCHARM -v 2.98.2 -d -s --config_file F2bc_paper_significance
 
-./plotter -b full.forCHARM -v 2.52.2 -d -s --config_file F2bc_paper_significance_mirrored
+./plotter -b full.forCHARM -v 2.98.2 -d -s --config_file F2bc_paper_significance_mirrored
