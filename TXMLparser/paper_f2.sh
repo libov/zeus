9d1895e878ad89e035ec352cbb982eb5ba27a6ce
Vladyslav <libov@mail.desy.de>
2013-11-03 20:43:49 +0100

    changed histogram version to 2.98.2 and 2.99.2 in extraction scripts

--- TXMLparser/paper_f2.sh
+++ TXMLparser/paper_f2.sh
@@ -1,3 +1,3 @@
-./f2bc --meta_file meta_Jun21_2012_09:45:50 --XMLfile results.full.forCHARM2.52.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_charm_correction_2.77QED_corrected.SYST --no_extraction_theory --draw_herapdf
+./f2bc --meta_file meta_Jun21_2012_09:45:50 --XMLfile results.full.forCHARM2.98.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_charm_correction_2.77QED_corrected.SYST --no_extraction_theory --draw_herapdf
 
-./f2bc --meta_file meta_Jun21_2012_09:54:42 --XMLfile results.full2.53.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_beauty_correction_2.78QED_corrected.SYST --beauty --no_extraction_theory --draw_herapdf
+./f2bc --meta_file meta_Jun21_2012_09:54:42 --XMLfile results.full2.99.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_beauty_correction_2.78QED_corrected.SYST --beauty --no_extraction_theory --draw_herapdf
--- TXMLparser/paper_reduced.sh
+++ TXMLparser/paper_reduced.sh
@@ -1,3 +1,3 @@
-./f2bc --meta_file meta_Jan6_2013_15:17:36 --XMLfile results.full.forCHARM2.52.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_charm_correction_2.77QED_corrected.SYST --grid_file q2_x_grid_only_extraction_points.txt --reduced
+./f2bc --meta_file meta_Jan6_2013_15:17:36 --XMLfile results.full.forCHARM2.98.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_charm_correction_2.77QED_corrected.SYST --grid_file q2_x_grid_only_extraction_points.txt --reduced
 
-./f2bc --meta_file meta_Jan6_2013_15:29:01 --XMLfile results.full2.53.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_beauty_correction_2.78QED_corrected.SYST --grid_file q2_x_grid_only_extraction_points.txt --reduced --beauty
+./f2bc --meta_file meta_Jan6_2013_15:29:01 --XMLfile results.full2.99.2.0405e06e07p.v02.true05e06e0607p.xmlcorrected.tracking_map_beauty_correction_2.78QED_corrected.SYST --grid_file q2_x_grid_only_extraction_points.txt --reduced --beauty
