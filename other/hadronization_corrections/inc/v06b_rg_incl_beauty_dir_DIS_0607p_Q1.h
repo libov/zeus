c3a3e36671dd23b646496ebeff848783ddcef635
Vladyslav <libov@mail.desy.de>
2013-12-30 08:23:58 +0100

    added include directory and added some Zacon classes there

---
+++ other/hadronization_corrections/inc/v06b_rg_incl_beauty_dir_DIS_0607p_Q1.h
@@ -0,0 +1,134 @@
+#include <string>
+#include <TChain.h>
+
+using namespace::std;
+
+class ZaconChainFactory_v06b_rg_incl_beauty_dir_DIS_0607p_Q1 {
+ 
+ public:
+
+  static TChain* addToZaconChain(TChain *ch) {
+    
+   string prefix="zeus://acs/z/ntup/07p/v06b/mc/root/";
+   ch->Add( (prefix + "zeusmc.afix627.f15643.rgap3.nc.b.q2g1.p.013.root").c_str() );
+   ch->Add( (prefix + "zeusmc.afix627.f15643.rgap3.nc.b.q2g1.p.058.root").c_str() );
+   ch->Add( (prefix + "zeusmc.ffix627.f15643.rgap3.nc.b.q2g1.p.011.root").c_str() );
+   ch->Add( (prefix + "zeusmc.ffix627.f15643.rgap3.nc.b.q2g1.p.015.root").c_str() );
+   ch->Add( (prefix + "zeusmc.ffix627.f15643.rgap3.nc.b.q2g1.p.037.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfir627.f15643.rgap3.nc.b.q2g1.p.114.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfir627.f15643.rgap3.nc.b.q2g1.p.115.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfir627.f15643.rgap3.nc.b.q2g1.p.116.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfir627.f15643.rgap3.nc.b.q2g1.p.117.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.001.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.002.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.003.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.004.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.005.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.006.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.007.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.008.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.009.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.016.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.017.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.018.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.019.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.020.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.021.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.022.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.023.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.024.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.025.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.026.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.027.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.028.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.029.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.030.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.031.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.032.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.033.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.034.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.035.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.038.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.039.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.040.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.041.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.042.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.043.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.044.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.045.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.046.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.047.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.048.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.049.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.050.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.051.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.052.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.053.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.054.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.055.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.056.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.059.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.060.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.061.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.062.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.063.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.064.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.065.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.066.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.067.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.068.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.069.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.070.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.071.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.072.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.073.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.074.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.075.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.076.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.077.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.078.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.079.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.080.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.081.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.082.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.083.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.084.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.085.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.086.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.087.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.088.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.089.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.090.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.091.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.092.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.093.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.094.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.095.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.096.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.097.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.098.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.099.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.100.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.101.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.102.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.104.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.105.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.106.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.107.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.108.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.109.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.110.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.111.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.112.root").c_str() );
+   ch->Add( (prefix + "zeusmc.hfix627.f15643.rgap3.nc.b.q2g1.p.113.root").c_str() );
+   ch->Add( (prefix + "zeusmc.ofix627.f15643.rgap3.nc.b.q2g1.p.010.root").c_str() );
+   ch->Add( (prefix + "zeusmc.ofix627.f15643.rgap3.nc.b.q2g1.p.014.root").c_str() );
+   ch->Add( (prefix + "zeusmc.ofix627.f15643.rgap3.nc.b.q2g1.p.103.root").c_str() );
+   ch->Add( (prefix + "zeusmc.xfix627.f15643.rgap3.nc.b.q2g1.p.012.root").c_str() );
+   ch->Add( (prefix + "zeusmc.xfix627.f15643.rgap3.nc.b.q2g1.p.036.root").c_str() );
+   ch->Add( (prefix + "zeusmc.xfix627.f15643.rgap3.nc.b.q2g1.p.057.root").c_str() );
+
+   return ch;
+  }
+
+};
